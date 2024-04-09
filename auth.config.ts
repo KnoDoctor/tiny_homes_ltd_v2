c42a0e8aa51beba846f79771d4db4e4de82f68fa
Jeff Barfield <barfieldjt@gmail.com>
2024-04-08 21:36:41 -0400

    removed logged in dashboard redirect

--- auth.config.ts
+++ auth.config.ts
@@ -11,20 +11,15 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      const isOnMyDashboard = nextUrl.pathname.startsWith('/my');
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       }
 
-      if (isOnMyDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      }
-
-      if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
+      // if (isLoggedIn) {
+      //   return Response.redirect(new URL('/dashboard', nextUrl));
+      // }
 
       return true;
     },
